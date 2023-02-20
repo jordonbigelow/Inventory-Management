6b2df0c475d57b7d107bf7bb53e8bf593fee1ff8
Jordon Bigelow <the-music-dude@hotmail.com>
2023-02-19 23:37:05 -0700

    added fields to ModifyProductController

--- src/main/java/code/Controllers/ModifyProductController.java
+++ src/main/java/code/Controllers/ModifyProductController.java
@@ -1,4 +1,13 @@
 package code.Controllers;
 
+import javafx.scene.control.TextField;
+
 public class ModifyProductController {
+    public TextField idField;
+    public TextField nameField;
+    public TextField inventoryField;
+    public TextField priceField;
+    public TextField minimumField;
+    public TextField maximumField;
+    public TextField searchField;
 }
