79970881c0208a91d50d72980bf19e829ab4a5c0
Jordon Bigelow <the-music-dude@hotmail.com>
2023-02-19 23:45:00 -0700

    set up modifyPartController fields and methods

--- src/main/java/code/Controllers/ModifyPartController.java
+++ src/main/java/code/Controllers/ModifyPartController.java
@@ -1,4 +1,32 @@
 package code.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class ModifyPartController {
+    public RadioButton inHouseRadio;
+    public ToggleGroup tGroup;
+    public RadioButton outsourcedRadio;
+    public Label changeableText;
+    public TextField idField;
+    public TextField nameField;
+    public TextField inventoryField;
+    public TextField priceField;
+    public TextField minimumField;
+    public TextField maximumField;
+    public TextField changeableField;
+    public Button saveButton;
+    public Button cancelButton;
+
+    public void handleInHouseRadioAction(ActionEvent actionEvent) {
+    }
+
+    public void handleOutsourcedRadioAction(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelButtonAction(ActionEvent actionEvent) {
+    }
 }
